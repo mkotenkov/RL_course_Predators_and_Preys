689a375a76b6477282a4bd938aee4aa0c43ef086
Maksim Koltiugin <mkoltugin@gmail.com>
2023-11-08 20:39:13 +0300

    new Reward

---
+++ modules/reward_NEW.py
@@ -0,0 +1,4 @@
+def calculate_state_value(processed_state_i, bonus_count,
+                          w_kill_prey, w_kill_enemy, w_kill_bonus,
+                          gamma_to_reduce_bonus_weight):
+    pass
