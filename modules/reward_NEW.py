2e96e3fc52ade65989db15f4d8ef559e303a52dd
Maksim Koltiugin <mkoltugin@gmail.com>
2023-11-08 20:45:07 +0300

    i will develop new reward now

--- modules/reward_NEW.py
+++ modules/reward_NEW.py
@@ -1,4 +1,7 @@
 def calculate_state_value(processed_state_i, bonus_count,
-                          w_kill_prey, w_kill_enemy, w_kill_bonus,
+                          ww_kill_prey, w_kill_enemy, w_kill_bonus,
                           gamma_to_reduce_bonus_weight):
-    pass
+    stones_mask, preys_mask, teammates_mask, enemies_mask, bonuses_mask = processed_state[
+        0, ...], processed_state[1, ...], processed_state[2, ...], 
+        processed_state[3, ...], processed_state[ 4, ...]
+        
